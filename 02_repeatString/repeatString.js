b0987c46988e80d47343037e32b51f928b317bd7
Dylan Lindsay <dylan234@tpg.com.au>
2022-09-07 10:40:06 +1000

    Completed repeatString exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let repeatedStr = "";
+  // for loop example
+  for (let i = 0; i < num; i++) {
+    repeatedStr += str;
+  }
+  if (num < 0) {
+    return "ERROR";
+  }
+  return repeatedStr;
 };
 
+// While loop example
+//   let index = 0;
+//   while (index < num) {
+//     repeatedStr += string;
+//     index++;
+//   }
+//   if (num < 0) {
+//     // returns ERROR if negative number
+//     return "ERROR";
+//   }
+//   return repeatedStr;
+// };
 // Do not edit below this line
 module.exports = repeatString;
