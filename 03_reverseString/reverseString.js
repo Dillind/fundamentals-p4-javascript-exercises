0ad2d8b383e3ec4eb43889aca1791d2b8768d14d
Dylan Lindsay <dylan234@tpg.com.au>
2022-09-07 17:28:16 +1000

    Exercise 3 completed.
    Learned how to reverse an array!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  // Step 1 - split the array
+  const splitArray = string.split("");
+  // Step 2 - reverse the array
+  const reverseArray = splitArray.reverse();
+  // Step 3 - join the array
+  const joinArray = reverseArray.join("");
+  // Step 4 - return the array
+  return joinArray;
 };
 
 // Do not edit below this line
