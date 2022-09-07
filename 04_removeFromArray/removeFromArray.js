9c0c65ac25c0d71683194737bc464484b464ec09
Dylan Lindsay <dylan234@tpg.com.au>
2022-09-07 18:17:00 +1000

    completed
    somewhat difficult. understand ForEach methods.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+// the rest paramter allows us to put an indefinite number of values into a parameter.
+const removeFromArray = function (...args) {
+  const array = args[0];
+  const newArray = [];
+  array.forEach((element) => {
+    if (!args.includes(element)) {
+      newArray.push(element);
+      console.log(newArray);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
