62c13a8fcec1d0fe16955a8f2c64bf562f0117ed
Dylan Lindsay <dylan234@tpg.com.au>
2022-09-11 17:57:12 +1000

    Completed exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+// fahrenheit to celsius
+const ftoc = function (fahren) {
+  const temp = ((fahren - 32) * 5) / 9;
+  return Math.round(temp * 10) / 10;
 };
 
-const ctof = function() {
-
+// celsius to fahrenheit
+const ctof = function (celsius) {
+  const temp = (celsius * 9) / 5 + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
